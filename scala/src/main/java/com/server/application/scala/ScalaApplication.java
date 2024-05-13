67bd312278f9b9c5200232954ef2367a9e577b72
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-05-13 15:44:10 +0530

    initialize project

---
+++ scala/src/main/java/com/server/application/scala/ScalaApplication.java
@@ -0,0 +1,13 @@
+package com.server.application.scala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalaApplication.class, args);
+	}
+
+}
---
+++ scala/src/test/java/com/server/application/scala/ScalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.application.scala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
