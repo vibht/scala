19fa00d201b91271a7e9f3f05c37b044dc5b3b95
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-06-21 13:21:06 +0530

    update thread class drop

--- scala/src/main/java/com/server/application/scala/ScalaApplication.java
+++ scala/src/main/java/com/server/application/scala/ScalaApplication.java
@@ -2,6 +2,7 @@ package com.server.application.scala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScalaApplication {
@@ -9,5 +10,7 @@ public class ScalaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScalaApplication.class, args);
 	}
+	
+	
 
 }
